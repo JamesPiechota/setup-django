a524e065930515fc536f9462126a6bf1b6ad9061
James Piechota <piechota@gmail.com>
2013-10-28 10:06:44 -0700

    Initialise heroku with a web and worker

--- new_project.py
+++ new_project.py
@@ -4,6 +4,7 @@ import subprocess
 import os
 import re
 import random
+import urllib
 
 
 class ShellScript(object):
@@ -20,8 +21,9 @@ class ShellScript(object):
         print self._temp_dir
         print self._script.name
         
-    def add(self, command, redirect=True):
-        self._script.write("echo %s\n" % (command))
+    def add(self, command, redirect=True, echo=True):
+        if echo:
+            self._script.write("echo %s\n" % (command))
         if redirect:
             self._script.write("%s >> %s\n" % (command, self._log_file))
         else:
@@ -78,6 +80,9 @@ def install_packages(script, config):
     script.add("pip install django-celery")
     script.add("pip install Scrapy")
     script.add("pip install -U celery-with-redis")
+    script.add("pip install django-toolbelt")
+    script.add("pip install newrelic")
+    
         
 def initialize_project(script, config):
     script.add_section("Create django project and local git repo")
@@ -85,10 +90,11 @@ def initialize_project(script, config):
     script.add("django-admin.py startproject %s" % config['project'])
     script.add("cd %s" % config['project'])
     script.add("git init")
+    script.add_template("django/.gitignore", ".")
     script.add_commit("Initial commit of %s" % config['project'])
     script.add_template("django/settings.py", config["project"])
     script.add_template("django/wsgi.py", config["project"], replace=config)
-    script.add("pip freeze > requirements.txt")
+    script.add("pip freeze > requirements.txt", redirect=False)
     script.add_commit("Install default settings.py, wsgi.py, and requirements.txt")
     
 def create_database(script, config):
@@ -104,8 +110,7 @@ def set_environment(script, config):
 def initialize_south(script, config):
     script.add_section("Initialize South") 
     script.add("python manage.py syncdb", redirect=False)
-    script.add("git add .")
-    script.add("git commit -a -m 'Added South for database migrations'")
+    script.add_commit("Added South for database migrations")
     
 def initialize_scrapy(script, config):
     script.add_section("Initialize scrapy")
@@ -118,18 +123,90 @@ def initialize_scrapy(script, config):
     script.add_template("scrapy/middleware.py", "scrape", replace=config)
     script.add_template("scrapy/items.py", "scrape", replace=config)
     script.add_template("scrapy/dmoz.py", "scrape/spiders", replace=config)
+    script.add_commit("Created a sample Scrapy project and set up integration with Django")
     
 def initialize_celery(script, config):
     script.add_section("Initialize Celery") 
     script.add("brew install redis")
     script.add("python manage.py migrate djcelery")
     script.add_template("celery/tasks.py", ".", replace=config)
+    script.add_commit("Created a default celery task and set up integration with Django")
     
+
+def initialize_heroku(script, config):
+    secret = secret_key()
+    
+    script.add_section("Initialize Heroku")
+    script.add("heroku apps:create %s" % config["heroku_web_app"])
+    
+    script.add("heroku addons:add heroku-postgresql --app %s" % config["heroku_web_app"])
+    script.add("pginfo=`heroku pg:info --app %s`" % config["heroku_web_app"], redirect=False, echo=False)
+    script.add("db=`echo $pginfo | sed 's/=== \([A-Z_]*\).*/\\1/'`", redirect=False, echo=False)
+    script.add("db_path=`heroku config:get $db --app %s`" % config["heroku_web_app"], redirect=False, echo=False)
+    script.add("heroku pg:promote $db --app %s" % config["heroku_web_app"])
+    script.add("heroku addons:add redistogo --app %s" % config["heroku_web_app"])
+    script.add("heroku addons:add newrelic --app %s" % config["heroku_web_app"])
+    
+    script.add("heroku apps:create %s" % config["heroku_worker_app"])
+    script.add("heroku addons:add redistogo --app %s" % config["heroku_worker_app"])
+    script.add("heroku addons:add newrelic --app %s" % config["heroku_worker_app"])
+    script.add("heroku config:set DATABASE_URL=$db_path --app %s" % config["heroku_worker_app"])
+    
+    heroku_set_env(config, config["heroku_web_app"], secret)
+    heroku_set_env(config, config["heroku_worker_app"], secret)
+    
+    script.add_template("heroku/Procfile", ".", replace=config)
+    script.add_commit("Installed default Heroku Procfile with web and worker tasks")
+    
+    script.add_section("Deploy to Heroku Web")
+    script.add("git remote add web git@heroku.com:%s.git" % config["heroku_web_app"])
+    script.add("ssh-add %s" % config["heroku_ssh"])
+    script.add("git push web master")
+    script.add("heroku run python manage.py syncdb --app %s" % config["heroku_web_app"], redirect=False)
+    script.add("heroku run python manage.py migrate djcelery --app %s" % config["heroku_web_app"])
+    script.add("heroku ps:scale web=1 --app %s" % config["heroku_web_app"])
+    script.add("heroku ps:scale worker=0 --app %s" % config["heroku_web_app"])
+    
+    script.add_section("Deploy to Heroku Worker")
+    script.add("git remote add worker git@heroku.com:%s.git" % config["heroku_worker_app"])
+    script.add("ssh-add %s" % config["heroku_ssh"])
+    script.add("git push worker master")
+    script.add("heroku ps:scale web=0 --app %s" % config["heroku_worker_app"])
+    script.add("heroku ps:scale worker=1 --app %s" % config["heroku_worker_app"])
+    
+def heroku_set_env(config, app, secret):
+    script.add("heroku config:set "
+               "TEMPLATE_DEBUG=DEBUG DEBUG=False SECRET_KEY='%s' DJANGO_PROJECT='%s' "
+               "EMAIL_HOST='smtp.mandrillapp.com' EMAIL_PORT='587' EMAIL_HOST_USER='%s' "
+               "EMAIL_HOST_PASSWORD='%s' EMAIL_USE_TLS=True --app %s" %
+               (secret, config["project"], config["mandrill_user"], config["mandrill_password"], app))
+    
+def heroku_app_exists(app):
+    return not re.search(r"Heroku \| No such app", urllib.urlopen("http://%s.herokuapp.com" % app).read())
+
+def prompt_for_heroku_app(config):
+    config["heroku_web_app"] = (raw_input("Please pick a name for your heroku app [%s]: "
+                                          % config["project"])
+                                or config["project"])
+    while heroku_app_exists(config["heroku_web_app"]):
+        config["heroku_web_app"] = (raw_input("'%s' already exists, please pick a different name [%s]: "
+                                              % (config["heroku_web_app"], config["project"])) 
+                                    or config["project"])
+            
+    config["heroku_worker_app"] = "%s-worker" % config["heroku_web_app"]
+    i = 1
+    while heroku_app_exists(config["heroku_worker_app"]):
+        config["heroku_worker_app"] = "%s-worker%s" % (config["heroku_web_app"], i)
+        i += 1
+    
+def secret_key():
+    return ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
     
 if __name__ == '__main__':
     script = ShellScript("setup.log")
     
     print "This script assumes you have a psql server installed and running"
+    print "This script assumes you have the heroku toolbelt installed an you're logged in"
     print "Type 'redis-server &' to run the installed redis server"
     
     config = {}
@@ -139,11 +216,13 @@ if __name__ == '__main__':
     config["heroku_db_password"] = "local1"
     config["mandrill_user"] = "james@nyle.co"
     config["mandrill_password"] = "5m1v3p2Y1Kzoivi_FF16dQ"
-    config["secret"] = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
+    config["secret"] = secret_key()
     config["scrapy_project"] = "scrape"
     config["scrapy_admin_email"] = "james@nyle.co"
     config["scrapy_from_email"] = "mail@nyle.co"
-    
+    config["heroku_ssh"] = "~/.ssh/heroku_nyle_rsa"
+    prompt_for_heroku_app(config)
+        
     setup_virtualenv(script, config)
     install_packages(script, config)
     initialize_project(script, config)
@@ -152,4 +231,5 @@ if __name__ == '__main__':
     initialize_south(script, config)
     initialize_scrapy(script, config)
     initialize_celery(script, config)
+    initialize_heroku(script, config)
     script.run()
